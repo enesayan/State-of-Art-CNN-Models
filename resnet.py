e407f9116b96f69698df11281452189624fb3faa
enesayan <enesayan@yandex.com>
2019-12-08 18:34:01 +0300

    Create resnet.py

---
+++ resnet.py
@@ -0,0 +1,3 @@
+from keras import layers
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+from keras.models import Model, load_model
