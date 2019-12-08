58a27c0ac4f611844f188de994e265edaeea8411
enesayan <enesayan@yandex.com>
2019-12-08 18:41:43 +0300

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -1,3 +1,48 @@
 from keras import layers
 from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
 from keras.models import Model, load_model
+
+stride=1
+CHANNEL_AXIS=3
+num_classes=10
+
+def residual_layer(x,filters,pooling=False, dropout=0.0):
+    temp = x
+    temp = Conv2D(filters,(3,3),strides = stride,padding = "same")(temp)
+    temp = BatchNormalization(axis = CHANNEL_AXIS)(temp)
+    temp = Activation("relu")(temp)
+    temp = Conv2D(filters,(3,3),strides = stride,padding = "same")(temp)
+
+    x = add([temp,Conv2D(filters,(3,3),strides = stride,padding = "same")(x)])
+    if pooling:
+        x = MaxPooling2D((2,2))(x)
+    if dropout != 0.0:
+        x = Dropout(dropout)(x)
+    x = BatchNormalization(axis = CHANNEL_AXIS)(x)
+    x = Activation("relu")(x)
+    return x
+ 
+
+img_input = Input(shape = (224,224,3))
+x = img_input
+x = Conv2D(16,(3,3),strides = stride,padding = "same")(x)
+x = BatchNormalization(axis = CHANNEL_AXIS)(x)
+x = Activation("relu")(x)
+x = res_layer(x,32,dropout = 0.2)
+x = res_layer(x,32,dropout = 0.3)
+x = res_layer(x,32,dropout = 0.4,pooling = True)
+x = res_layer(x,64,dropout = 0.2)
+x = res_layer(x,64,dropout = 0.2,pooling = True)
+x = res_layer(x,256,dropout = 0.4)
+x = Flatten()(x)
+x = Dropout(0.4)(x)
+x = Dense(4096,activation = "relu")(x)
+x = Dropout(0.23)(x)
+x = Dense(num_classes,activation = "softmax")(x)
+
+resnet_model = Model(img_input,x,name = "Resnet")
+
+resnet_model.summary()
+
+
+# __________________________________________________________________________________________________
