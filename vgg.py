1ec51e71e30abd90aa818fd3c70712836d3b38fb
enesayan <enesayan@yandex.com>
2019-12-08 12:31:33 +0300

    Update and rename vgg.py to vgg16.py

--- vgg.py
+++
@@ -1,7 +0,0 @@
-from keras.layers import Input, Dense, Flatten
-from keras.layers import Conv2D,MaxPooling2D,Dropout
-from keras.models import Model
-
-
-input_img = Input(shape=(224, 224, 3))
-x=Conv2D(32,(3,3), strides=1,padding="same",activation="relu",input_shape=input(img)
